34d897687adb700f08d928bfa9c8556c0d41a9a1
kunal9711 <kkunalkumar112@gmail.com>
2022-08-02 15:09:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Slides from './components/Slides';
+
+
+function App({slides}) {
+
+    return (
+        <div>
+            
+            <div className="App">
+                <Slides
+                    slides={slides}/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
